e0f4c1c388e65582ec92516c32ed7e39e60a02f0
Karel Horsky <karel.horsky@centrum.cz>
2017-08-04 15:10:33 +0200

    tests: remove dependency on RobotLoader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,25 +2,9 @@
 
 declare(strict_types = 1);
 
-use Nette\Loaders\RobotLoader;
 use Tester\Environment;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 Environment::setup();
 date_default_timezone_set('Europe/Prague');
-
-define('TEMP_DIR', __DIR__ . '/../temp/');
-
-if (!@mkdir(TEMP_DIR) && !is_dir(TEMP_DIR)) {
-	throw new \RuntimeException(sprintf('Unable to create temporary directory (%s)!', TEMP_DIR));
-}
-
-$loader = new RobotLoader;
-$loader
-	->setTempDirectory(TEMP_DIR)
-	->setAutoRefresh()
-	->addDirectory(__DIR__)
-	->addDirectory(__DIR__ . '/../src');
-
-return $loader->register();
